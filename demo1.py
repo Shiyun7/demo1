5f7a87efa7676114109d89cbb2963139438b0253
Shiyun <l>
2021-09-25 20:38:40 +0800

    第一次向GitHub提交代码

---
+++ demo1.py
@@ -0,0 +1,25 @@
+"""
+print("hello world")
+print("xixi"+"hh")
+#print("hhh"*100)  #打印100个hhh
+print(8>9)
+
+"""
+# a = int(input("请输入："));#input输入的都是字符串
+
+# b = int(input("请输入："))
+# print("获取的内容：",a+b)
+
+# c = "sdfjsdjfsf"
+# print(len(c))
+#数据格式的转换
+# print(type(()))
+
+#练习通过代码获取两段内容，并且计算他们的长度之和
+a = input("请输入：")
+b = input("请输入：")
+print("两个字符串的综合为：",len(a+b))
+
+
+
+
